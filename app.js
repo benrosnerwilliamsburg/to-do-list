5fa3d69cce7c1c39e4ec707f47a74d1688cb6b4a
br305893 <ben.rosner.williamsburg@gmail.com>
2020-11-22 14:52:11 -0500

    fix event listener bug

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 function newItem() {
-    console.log("Inside newItem");
     let item = document.getElementById("input").value;
-    console.log(item)
-
-    // store the unordered list as a variable (now we can refer to it as "ul")
-    const ul = document.getElementById("list");
-
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode("- " + item)); // now put text in list item
+    let ul = document.getElementById("list");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode("- " + item));
     ul.appendChild(li);
 
-    document.getElementById("input").value = ""; // erase what is currently in todo list
+    document.getElementById("input").value = "";
 
-    li.onclick = removeItem; // run removeItem when the li is clicked
+    li.onclick = removeItem;
 }
 
 
 document.body.onkeyup = function(e) {
-    if (e.keyCode = 13) {
-        console.log("enter clicked")
+    if (e.keyCode == 13) {
         newItem();
     }
   };
 
 function removeItem(e) {
-    e.target.remove()
+    e.target.parentElement.removeChild(e.target);
 }
